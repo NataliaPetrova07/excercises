fe4baa88326ff7f99f4f79606a187d472feab79d
natalia <nata774e@stud.kea.dk>
2022-09-01 11:35:54 +0200

    for loop

---
+++ forloop.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// for (let counter = 10; counter >= 0; counter--) {
+// console.log(counter);
+// if (counter === 0) {
+//     console.log("liftoff");
+//  }
+// }
+//
+
+// for (let counter = 1; counter < 20; counter+=2) {
+//   console.log(`counter is ${counter}` );
+// }
+
+// for (let counter = 1; counter <= 16777216; counter*=2) {
+//     console.log(`counter is ${counter}` );
+//   }
+
+// for (let counter = 111; counter < 138; counter += 3) {
+//   console.log(`counter is ${counter}`);
+// }
+
+for (let counter = 100; counter > 0; counter -= 10) {
+  console.log(`counter is ${counter}`);
+}
