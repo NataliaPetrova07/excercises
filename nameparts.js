5e0cb7497e89c9dbca6d72d3f3eaadcbd0c0a4e6
natalia <nata774e@stud.kea.dk>
2022-09-05 16:08:41 +0200

    Exercise - cssToRGB function

--- nameparts.js
+++ nameparts.js
@@ -14,8 +14,6 @@ let lastName = stringName.substring(stringName.lastIndexOf(" "));
 
 console.log(firstName, middleName, lastName);*/
 
-
-
 // const fullname = "Peter Bosch Lind";
 
 // function getNameParts(stringName) {
@@ -30,16 +28,26 @@ console.log(firstName, middleName, lastName);*/
 
 // getNameParts(fullname);
 
+// function fullName(lastName, firstName, middleName, gibberish) {
+//   firstName += " ";
+//   if (middleName === undefined) {
+//     middleName = "";
+//   } else {
+//     middleName += " ";
+//   }
+//   console.log(`${firstName}${middleName}${lastName}`);
+// }
 
+// fullName("Potter", "Harry", "James", "Pottypotpot");
 
-function fullName(lastName, firstName, middleName, gibberish) {
-  firstName += " ";
-  if (middleName === undefined) {
-    middleName = "";
-  } else {
-    middleName += " ";
-  }
-  console.log(`${firstName}${middleName}${lastName}`);
+let cssString = "rgb(192, 13, 1)";
+
+function cssToRGB(string) {
+  let r = parseInt(string.substring(4, string.indexOf(",")));
+  let g = parseInt(string.substring(string.indexOf(" ") + 1, string.lastIndexOf(",")));
+  let b = parseInt(cssString.substring(string.lastIndexOf(" ") + 1, string.indexOf(")")));
+
+  console.log(r, g, b);
 }
 
-fullName("Potter", "Harry", "James", "Pottypotpot");
+cssToRGB(cssString);
