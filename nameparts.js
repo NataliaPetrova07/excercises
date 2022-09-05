a94eb028e17734ba830fbaeadbe93b4f6896800a
natalia <nata774e@stud.kea.dk>
2022-09-05 14:55:33 +0200

    spaces before words - fixed with indexOf + 1

--- nameparts.js
+++ nameparts.js
@@ -14,13 +14,13 @@ let lastName = stringName.substring(stringName.lastIndexOf(" "));
 
 console.log(firstName, middleName, lastName);*/
 
-const fullname = "Peter Lind";
+const fullname = "Peter Bosch Lind";
 
 function getNameParts( stringName ){
     let firstName = stringName.substring(0, stringName.indexOf(" "));
-    let middleName = stringName.substring(stringName.indexOf(" "), stringName.lastIndexOf(" "));
-    let lastName = stringName.substring(stringName.lastIndexOf(" "));
-    if(middleName === ""){
+    let middleName = stringName.substring(stringName.indexOf(" ") + 1, stringName.lastIndexOf(" "));
+    let lastName = stringName.substring(stringName.lastIndexOf(" ") + 1);
+    if(middleName === " "){
         middleName = undefined;
     };
     console.log({firstName, middleName, lastName });
